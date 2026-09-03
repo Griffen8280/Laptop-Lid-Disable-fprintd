5eaa9a06347551371dbe50fbed916964af885668
Chris P. <42878642+Griffen8280@users.noreply.github.com>
2026-09-03 09:15:20 -0400

    Fix comment formatting in laptop-lid.sh

---
+++ laptop-lid.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+lock=$HOME/fprint-disabled
+
+if grep -Fq closed /proc/acpi/button/lid/*/state #&&
+  #grep -Fxq connected /sys/class/drm/card0-HDMI-A-1/status
+#Uncomment the && and the grep line to enable a secondary check for external display connection
+then
+  touch "$lock"
+  systemctl stop fprintd
+  systemctl mask fprintd
+elif [ -f "$lock" ]
+then
+  systemctl unmask fprintd
+  systemctl start fprintd
+  rm "$lock"
+fi
