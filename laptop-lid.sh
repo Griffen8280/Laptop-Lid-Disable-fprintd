f0e3201696bc3cf0d066e4c1c642d15cee609a90
Chris P. <42878642+Griffen8280@users.noreply.github.com>
2026-09-03 10:06:41 -0400

    Modify laptop-lid.sh for continuous lid state monitoring
    Refactor laptop-lid.sh to run in an infinite loop for continuous lid state checking.

--- laptop-lid.sh
+++ laptop-lid.sh
@@ -2,16 +2,18 @@
 
 lock=$HOME/fprint-disabled
 
-if grep -Fq closed /proc/acpi/button/lid/*/state #&&
-  #grep -Fxq connected /sys/class/drm/card0-HDMI-A-1/status
-#Uncomment the && and the grep line to enable a secondary check for external display connection
-then
-  touch "$lock"
-  systemctl stop fprintd
-  systemctl mask fprintd
-elif [ -f "$lock" ]
-then
-  systemctl unmask fprintd
-  systemctl start fprintd
-  rm "$lock"
-fi
+while true; do
+    if grep -Fq closed /proc/acpi/button/lid/*/state #&&
+       #grep -Fxq connected /sys/class/drm/card0-HDMI-A-1/status
+    #Uncomment the && and the grep line to enable a secondary check for external display connection
+    then
+      touch "$lock"
+      systemctl stop fprintd
+      systemctl mask fprintd
+    elif [ -f "$lock" ]
+    then
+      systemctl unmask fprintd
+      systemctl start fprintd
+      rm "$lock"
+    fi
+done
